e40176a6890ed1e06f7b5300cddbdd949a01d3dd
Jeremy Leonardo <58095255+jeremyleonardo@users.noreply.github.com>
2023-08-20 14:46:56 +0700

    added sfx on score up and lose

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Member;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public AudioClip deadAudio;
+    public AudioClip scoreUpAudio;
+
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     [ContextMenu("Increase Score")]  // will add this func to the unity's context menu
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+        audioSource.PlayOneShot(scoreUpAudio);
     }
 
     public void restartGame(){
@@ -23,5 +34,6 @@ public class LogicScript : MonoBehaviour
 
     public void gameOver(){
         gameOverScreen.SetActive(true);
+        audioSource.PlayOneShot(deadAudio);
     }
 }
